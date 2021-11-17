223414f7412c886e24dd0af97f007e3404c8a3a6
ShalaevRoman <mc.shel24@gmail.com>
2021-11-17 16:58:21 +0200

    modifications to the iterator pattern

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-let btn_prev = document.querySelector(".prev"),
-    btn_next = document.querySelector(".next"),
-    images = document.querySelectorAll(".pictures img");
-    i = 0;
-
-function next() {
-  images[i].style.display = "none";
-  i++;
-  if(i >= images.length){
-    i = 0;
+class carousel {
+  constructor(arr) {
+    this.arr = arr;
   }
-  images[i].style.display = "block";
-};
 
-function prev() {
-  images[i].style.display = "none";
-  i = i - 1;
-  if(i < 0) {
-    i = images.length - 1;
+  next() {
+    let index = 0,
+
+    return {
+      next: function () {
+        let element;
+        element = this.arr[index];
+        index++;
+        return element
+      }
+    }    
   }
-  images[i].style.display = "block";
 }
-
-btn_next.onclick = next;
-btn_prev.onclick = prev;
 
 
 
